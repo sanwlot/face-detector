044431822c62a40fe5af8fb17fa50f5c7e7b6994
sunnySavlot <sunny.savlot@gmail.com>
2023-10-13 13:37:22 +0530

    added placeholder for the input and description

--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -5,11 +5,12 @@ export default function ImageLinkForm({ onInputChange, onButtonSubmit }) {
   return(
     <div>
       <p className="f3 center text-color ph5" >
-        {'This magic brain will detect faces in your pictures, Give it a try.'}
+        This App will detect face in the picture. Put any image link in the input box, Give it a try.
       </p>
       <div className="center"> 
       <div className='form center pa4 br3 shadow-5'>
         <input 
+          placeholder='Enter image link...'
           className=" f4 p2 w-70 center" 
           type="text" 
           onChange={onInputChange}
