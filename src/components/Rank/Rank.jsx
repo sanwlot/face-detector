abeb5f58fb094df4eb4b04e57f562b669a225069
sunnySavlot <sunny.savlot@gmail.com>
2023-12-19 22:18:11 +0530

    updated gh-page link

--- src/components/Rank/Rank.jsx
+++ src/components/Rank/Rank.jsx
@@ -1,14 +1,12 @@
-import './Rank.css'
+import "./Rank.css";
 
-export default function Rank({name, entries}) {
-  return(
+export default function Rank({ name, entries }) {
+  return (
     <div>
-      <div className='white f3 center ph5'>
+      <div className="white f3 center ph5">
         {`${name}, your current entry count is...`}
       </div>
-      <div className="f1 white center">
-        {`#${entries}`}
-      </div>
+      <div className="f1 white center">{`#${entries}`}</div>
     </div>
-  )
+  );
 }
